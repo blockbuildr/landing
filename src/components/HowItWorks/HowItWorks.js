9e9e97d57904620a8465864ea636aa965b0e46e5
daniel <danieljoonlee@gmail.com>
2018-05-29 22:10:06 -0700

    prettier

--- src/components/HowItWorks/HowItWorks.js
+++ src/components/HowItWorks/HowItWorks.js
@@ -8,113 +8,109 @@ import Button from '../Button';
 
 const HowItWorks = ({classes}) => {
   return (
-      <Grid
-        className={classes.gridContainer}
-        container
-        direction="column"
-      >
+    <Grid className={classes.gridContainer} container direction="column">
+      <Grid item>
+        <Typography variant="title" className={classes.title}>
+          How it Works
+        </Typography>
+      </Grid>
+
+      <Grid container direction="row" className={classes.blockbuildr}>
         <Grid item>
-          <Typography variant="title" className={classes.title}>
-            How it Works
-          </Typography>
+          <div className={classes.placeholder} />
         </Grid>
 
-        <Grid
-          container
-          direction="row"
-          className={classes.blockbuildr}
-        >
-          <Grid item>
-            <div className={classes.placeholder} />
-          </Grid>
-
-          <Grid item className={classes.card}>
-            <Grid container direction="column" className={classes.rightText}>
-              <Grid item>
-                <Typography variant="title" className={classes.subheader}>
-                  Sub-header split onto 2 line. It’s really great. Lorem ipsum.
-                </Typography>
-              </Grid>
-
-              <Grid item className={classes.text}>
-                <Typography className={classes.description}>
-                  Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis.
-                </Typography>
-                <Typography className={classes.description}>
-                  Nulla facilisi. Ut fringilla. Sed nisi. Nulla quis sem at nibh elementum imperdiet.
-                </Typography>
-                <Typography className={classes.description}>
-                  Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa.  Nunc feugiat mi a tellus consequat imperdiet.
-                </Typography>
-              </Grid>
+        <Grid item className={classes.card}>
+          <Grid container direction="column" className={classes.rightText}>
+            <Grid item>
+              <Typography variant="title" className={classes.subheader}>
+                Sub-header split onto 2 line. It’s really great. Lorem ipsum.
+              </Typography>
             </Grid>
-          </Grid>
-        </Grid>
 
-        <Grid
-          container
-          direction="row"
-          className={classes.blockbuildr}
-        >
-          <Grid item className={classes.card}>
-            <Grid container direction="column" className={classes.leftText}>
-              <Grid item>
-                <Typography variant="title" className={classes.subheader}>
-                  Sub-header split onto 2 line. It’s really great. Lorem ipsum.
-                </Typography>
-              </Grid>
-
-              <Grid item className={classes.text}>
-                <Typography className={classes.description}>
-                  Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis.
-                </Typography>
-                <Typography className={classes.description}>
-                  Nulla facilisi. Ut fringilla. Sed nisi. Nulla quis sem at nibh elementum imperdiet.
-                </Typography>
-                <Typography className={classes.description}>
-                  Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa.  Nunc feugiat mi a tellus consequat imperdiet.
-                </Typography>
-              </Grid>
-            </Grid>
-          </Grid>
-
-          <Grid item>
-            <div className={classes.placeholder} />
-          </Grid>
-        </Grid>
-
-        <Grid
-          container
-          direction="row"
-          className={classes.blockbuildr}
-        >
-          <Grid item>
-            <div className={classes.placeholder} />
-          </Grid>
-
-          <Grid item>
-            <Grid container direction="column" className={classes.rightText}>
-              <Grid item>
-                <Typography variant="title" className={classes.subheader}>
-                  Sub-header split onto 2 line. It’s really great. Lorem ipsum.
-                </Typography>
-              </Grid>
-
-              <Grid item className={classes.text}>
-                <Typography className={classes.description}>
-                  Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis.
-                </Typography>
-                <Typography className={classes.description}>
-                  Nulla facilisi. Ut fringilla. Sed nisi. Nulla quis sem at nibh elementum imperdiet.
-                </Typography>
-                <Typography className={classes.description}>
-                  Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa.  Nunc feugiat mi a tellus consequat imperdiet.
-                </Typography>
-              </Grid>
+            <Grid item className={classes.text}>
+              <Typography className={classes.description}>
+                Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor
+                neque adipiscing diam, a cursus ipsum ante quis turpis.
+              </Typography>
+              <Typography className={classes.description}>
+                Nulla facilisi. Ut fringilla. Sed nisi. Nulla quis sem at nibh
+                elementum imperdiet.
+              </Typography>
+              <Typography className={classes.description}>
+                Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue
+                semper porta. Mauris massa. Nunc feugiat mi a tellus consequat
+                imperdiet.
+              </Typography>
             </Grid>
           </Grid>
         </Grid>
       </Grid>
+
+      <Grid container direction="row" className={classes.blockbuildr}>
+        <Grid item className={classes.card}>
+          <Grid container direction="column" className={classes.leftText}>
+            <Grid item>
+              <Typography variant="title" className={classes.subheader}>
+                Sub-header split onto 2 line. It’s really great. Lorem ipsum.
+              </Typography>
+            </Grid>
+
+            <Grid item className={classes.text}>
+              <Typography className={classes.description}>
+                Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor
+                neque adipiscing diam, a cursus ipsum ante quis turpis.
+              </Typography>
+              <Typography className={classes.description}>
+                Nulla facilisi. Ut fringilla. Sed nisi. Nulla quis sem at nibh
+                elementum imperdiet.
+              </Typography>
+              <Typography className={classes.description}>
+                Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue
+                semper porta. Mauris massa. Nunc feugiat mi a tellus consequat
+                imperdiet.
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+
+        <Grid item>
+          <div className={classes.placeholder} />
+        </Grid>
+      </Grid>
+
+      <Grid container direction="row" className={classes.blockbuildr}>
+        <Grid item>
+          <div className={classes.placeholder} />
+        </Grid>
+
+        <Grid item>
+          <Grid container direction="column" className={classes.rightText}>
+            <Grid item>
+              <Typography variant="title" className={classes.subheader}>
+                Sub-header split onto 2 line. It’s really great. Lorem ipsum.
+              </Typography>
+            </Grid>
+
+            <Grid item className={classes.text}>
+              <Typography className={classes.description}>
+                Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor
+                neque adipiscing diam, a cursus ipsum ante quis turpis.
+              </Typography>
+              <Typography className={classes.description}>
+                Nulla facilisi. Ut fringilla. Sed nisi. Nulla quis sem at nibh
+                elementum imperdiet.
+              </Typography>
+              <Typography className={classes.description}>
+                Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue
+                semper porta. Mauris massa. Nunc feugiat mi a tellus consequat
+                imperdiet.
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 };
 
