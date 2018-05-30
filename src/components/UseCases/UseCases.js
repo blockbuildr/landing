0801e738a54ec5784c3104d17c427c0c14d6cd99
danieljoonlee <danieljoonlee@gmail.com>
2018-05-30 00:08:56 -0700

    use cases (#30)
    * update to hi-fi mock ups
    
    * prettier
    
    * add what is blockbuildr detail
    
    * update button
    
    * features
    
    * how it works
    
    * white paper
    
    * use cases

---
+++ src/components/UseCases/UseCases.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {withStyles} from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '../Typography';
+import styles from './styles';
+import Card from './Card';
+
+const UseCases = ({classes}) => {
+  return (
+    <Grid className={classes.gridContainer} container direction="column">
+      <Grid item>
+        <Typography variant="title" className={classes.title}>
+          Use Cases
+        </Typography>
+      </Grid>
+
+      <Grid item>
+        <Grid container direction="row">
+          <Grid item>
+            <Card header="Collectibles" />
+          </Grid>
+          <Grid item className={classes.card}>
+            <Card header="Reward Programs" />
+          </Grid>
+          <Grid item className={classes.card}>
+            <Card header="Crowdfunding" />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
+
+UseCases.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(UseCases);
